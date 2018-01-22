1dd6b71583fae34423fc433bc4c10b4d88ec18ec
alex <geniofuturo@gmail.com>
2018-01-22 17:13:44 -0200

    Plugin installer.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xadmin-plugin-guardian',
+    version='1.0.0',
+    packages=['xplugin_guardian'],
+    url='https://github.com/alexsilva/xadmin.plugin.guardian',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='Plugin that implement the guardian support in xadmin.'
+)
