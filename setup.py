e08a6942816c9d0874f5aa47469b2e81baa2aa61
alex <geniofuturo@gmail.com>
2018-01-26 15:46:13 -0200

    Setting 'django-guardian' as dependency.

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='xadmin-plugin-guardian',
     version='1.0.0',
     include_package_data=True,
+    install_requires=[
+        'django-guardian'
+    ],
     packages=['xplugin_guardian'],
     url='https://github.com/alexsilva/xadmin.plugin.guardian',
     license='MIT',
