80742e0c4953a3192717d0e1e60d44d0f651824d
alexsilva <geniofuturo@gmail.com>
2021-08-12 16:36:09 -0300

    pep8

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='xadmin-guardian-plugin',
-    version='1.0.0',
-    include_package_data=True,
-    install_requires=[
-        'django-guardian'
-    ],
-    packages=['xplugin_guardian', "xplugin_guardian.templatetags"],
-    url='https://github.com/alexsilva/xadmin.plugin.guardian',
-    license='MIT',
-    author='alex',
-    author_email='alex@fabricadigital.com.br',
-    description='Plugin that implement the guardian support in xadmin.'
+	name='xadmin-guardian-plugin',
+	version='1.0.0',
+	include_package_data=True,
+	install_requires=[
+		'django-guardian'
+	],
+	packages=['xplugin_guardian', "xplugin_guardian.templatetags"],
+	url='https://github.com/alexsilva/xadmin.plugin.guardian',
+	license='MIT',
+	author='alex',
+	author_email='alex@fabricadigital.com.br',
+	description='Plugin that implement the guardian support in xadmin.'
 )
