578f24c1b950d8cafff8964f12b872d22e3dc263
alexsilva <geniofuturo@gmail.com>
2021-08-12 13:25:05 -0300

    Editing existing permissions.

--- xplugin_guardian/templatetags/xguardian.py
+++ xplugin_guardian/templatetags/xguardian.py
@@ -6,4 +6,18 @@ register = template.Library()
 
 @register.simple_tag
 def rows_range(counter, total):
-    return range(total - counter - 1)
+	return range(total - counter - 1)
+
+
+@register.simple_tag(takes_context=True)
+def permission_edit_url(context, obj):
+	admin_view = context['admin_view']
+	opts = obj._meta
+	url = admin_view.get_admin_url(
+		f"guardian_permissions_{opts.model_name}",
+		context['app_label'],
+		context['opts'].model_name,
+		context['object'].pk,
+		obj.pk
+	)
+	return url
